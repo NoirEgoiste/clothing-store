bad82db63b3e05bf1a51752389491eb8ac206935
Noir_Egoiste <dimon0.0@yandex.ru>
2024-01-03 17:49:55 +0300

    add password reset and sent token.

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,41 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
     path("register", views.register, name="register"),
+    path("login", views.login, name="login"),
+    path("logout", views.logout, name="logout"),
+    path("dashboard", views.dashboard, name="dashboard"),
+    path("delete-account", views.delete_account, name="delete-account"),
+    path(
+        "profile-management",
+        views.profile_management,
+        name="profile-management",
+    ),
+    # Password management.
+    path(
+        "reset-password",
+        auth_views.PasswordResetView.as_view(),
+        name="reset-password",
+    ),
+    path(
+        "reset-password-sent",
+        auth_views.PasswordResetDoneView.as_view(),
+        name="password-reset-done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(),
+        name="password-reset-confirm",
+    ),
+    path(
+        "reset-password-complete",
+        auth_views.PasswordResetCompleteView.as_view(),
+        name="reset-password-complete",
+    ),
+    # Email verification.
     path(
         "email-verification/<str:uidb64>/<str:token>/",
         views.email_verification,
